85a78c305c0245fdcc0926dba968395763581c3d
sarasinghrse <20singhsara@gmail.com>
2025-08-21 17:40:46 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,13 +32,14 @@ app.use(cors(
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
 
-if(process.env.NODE_ENV=="production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../frontend/dist");
+  app.use(express.static(frontendPath));
 
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"../frontend/dist","index.html"));
-  })
-
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
 }
 
 server.listen(PORT,()=>{
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -34,4 +34,4 @@ io.on("connection", (socket) => {
   });
 });
 
-export { io, app, server };
+export { io, app, server ,userSocketMap};
