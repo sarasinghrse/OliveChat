96de9b2939bc021696a44ad1661a93471c492503
sarasinghrse <20singhsara@gmail.com>
2025-08-21 17:55:58 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
+import express from 'express';
 
 dotenv.config();
 
