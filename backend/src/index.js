baf9b02f17d44f2f1846b8217bf1296b4b591e4c
sarasinghrse <20singhsara@gmail.com>
2025-08-21 22:18:25 +0530

    final final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5002;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://olive-chat.vercel.app",
   credentials: true,
 }));
 
